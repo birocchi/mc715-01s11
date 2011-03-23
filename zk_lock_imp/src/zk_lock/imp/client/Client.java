ef95ac13bd328f774a83b31ab8dd4f43e4eb2722
rgcano <rgcano@aa922603-347c-9e05-6bf4-27d00d4c72f5>
2011-03-23 07:47:10 +0000

    bug corrigido, programa interativo do andre funcionando

--- zk_lock_imp/src/zk_lock/imp/client/Client.java
+++ zk_lock_imp/src/zk_lock/imp/client/Client.java
@@ -2,6 +2,7 @@ package zk_lock.imp.client;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
@@ -12,170 +13,183 @@ import org.apache.zookeeper.recipes.lock.WriteLock;
 
 public class Client extends Thread implements Watcher
 {
-	private ZooKeeper zk;
-	private volatile boolean dead;	
+    private ZooKeeper zk;
+    private volatile boolean dead;
+    private WriteLock lock = null;
 	
-	/* properties */
+    /* properties */
 	
-	public boolean getDead()
-	{
-		return dead;
-	}
+    public boolean getDead()
+    {
+	return dead;
+    }
 
-	/* constructor */
+    /* constructor */
 	
-	public Client(String[] hosts, int port) throws IOException
-	{
-		StringBuilder connString = new StringBuilder();
+    public Client(String[] hosts, int port) throws IOException
+    {
+	StringBuilder connString = new StringBuilder();
 		
-		for(String h : hosts)
-			connString.append(h + ":").append(port).append(",");
+	for(String h : hosts)
+	    connString.append(h + ":").append(port).append(",");
 		
-		connString.setLength(connString.length() - 1);		
+	connString.setLength(connString.length() - 1);		
 		
-		dead = false;		
-		zk = new ZooKeeper(connString.toString(), 3000, this);		
-	}
+	dead = false;		
+	zk = new ZooKeeper(connString.toString(), 3000, this);		
+    }
 		
-	@Override
+    @Override
 	public void process(WatchedEvent event)
-	{	
-		if (event.getType() == Event.EventType.None)
-		{
-			// estado da conexao mudou
-			switch(event.getState())
-			{
-				case SyncConnected:
-					// conectamos
-					// podemos iniciar					
-					break;
+    {	
+	if (event.getType() == Event.EventType.None)
+	    {
+		// estado da conexao mudou
+		switch(event.getState())
+		    {
+		    case SyncConnected:
+			// conectamos
+			// podemos iniciar					
+			break;
 					
-				case Expired:
-					// sessao expirada
-					dead = true;
-					break;
-			}
-		}
-		else
-		{
-			// algo no nó mudou
-		}
-	}
+		    case Expired:
+			// sessao expirada
+			dead = true;
+			break;
+		    }
+	    }
+	else
+	    {
+		// algo no nó mudou
+	    }
+    }
 	
-	/**
-	 * Starts this client.
-	 */
-	public void run()
-	{				
-		BufferedReader stdin = new BufferedReader(new java.io.InputStreamReader(System.in));
-		String line;
+    /**
+     * Starts this client.
+     */
+    public void run()
+    {				
+	BufferedReader stdin = new BufferedReader(new java.io.InputStreamReader(System.in));
+	String line;
 		
-		System.out.println("Esperando conexão...");
+	System.out.println("Esperando conexão...");
 				
-		try
-		{
-			while (zk.getState() != States.CONNECTED)
-			sleep(500);
-		} 
-		catch (InterruptedException e1)
-		{
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+	try
+	    {
+		while (zk.getState() != States.CONNECTED)
+		    sleep(500);
+	    } 
+	catch (InterruptedException e1)
+	    {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	    }
 		
-		System.out.println("Conexão estabelecida.");
+	System.out.println("Conexão estabelescida.");
 		
-		try
-		{
-			while (!(line = stdin.readLine().toLowerCase()).equals("quit"))
-			{
-				String[] args = line.split(" "); 
-				String cmd = args[0];
+	try
+	    {
+		while ((line = stdin.readLine().toLowerCase()) != "quit")
+		    {
+			String[] args = line.split(" "); 
+			String cmd = args[0];
 				
-				if (args.length < 2)
-				{
-					System.out.println("Usage:");
-					System.out.println("quit");
-					System.out.println("[lock | unlock] /lockname");
-				}
-				else
-				{
-					String arg = args[1];
+			if (args.length < 2)
+			    {
+				System.out.println("Usage:");
+				System.out.println("[lock | unlock] /lockname");
+			    }
+			else
+			    {
+				String arg = args[1];
 					
-					if (arg.charAt(0) != '/')
-						System.out.println("Lock deve começar com /");
-					else					
-					{
-						if (cmd.equals("lock"))
-						{					
-							lock(arg);
+				if (arg.charAt(0) != '/')
+				    System.out.println("Lock deve começar com /");
+				else					
+				    {
+					if (cmd.equals("lock"))
+					    {					
+						lock(arg);
+					    }
+					else if (cmd.equals("unlock"))
+					    {
+						unlock(arg);
+					    }
+					else if (cmd.equals("children"))
+					    {
+						try{
+						    List<String> children = zk.getChildren(arg, false);
+						    while(!children.isEmpty()){
+							String cname = children.get(0);
+							children.remove(0);
+							System.out.println(cname);
+						    }
+						} catch (Exception e){
+						    e.printStackTrace();
 						}
-						else if (cmd.equals("unlock"))
-						{
-							unlock(arg);
-						}	
-					}
-				}
-			}
-		} 
-		catch (IOException e)
-		{
+					    }
+					else if(cmd.equals("owner"))
+					    {
+						System.out.println(lock.isOwner());
+					    }
+				    }
+			    }
+		    }
+	    } 
+	catch (IOException e)
+	    {
+		e.printStackTrace();
+	    }
+		
+	//		try
+	//		{
+	//			
+	//		} 
+	//		catch (KeeperException e)
+	//		{
+	//			e.printStackTrace();
+	//		} 
+	//		catch (InterruptedException e)
+	//		{
+	//			e.printStackTrace();
+	//		}
+    }
+
+    private void unlock(String arg)
+    {
+	if(lock == null)
+	    return;
+		
+	System.out.println("Liberando lock " + arg + "...");
+	lock.unlock();
+	System.out.println("Lock " + arg + " liberado");
+    }
+
+    private void lock(String arg)
+    {
+	lock = new WriteLock(zk, arg, null);
+		
+	if (lock.isOwner())
+	    System.out.println("Você já possui o lock " + arg);
+	else
+	    {
+		try
+		    {
+			System.out.println("Obtendo lock " + arg + "...");
+				
+			if (lock.lock())
+			    System.out.println("Lock " + arg + " obtido com sucesso");
+			else
+			    System.out.println("Lock " + arg + " não foi obtido");
+		    } 
+		catch (KeeperException e)
+		    {
 			e.printStackTrace();
-		}
-		
-//		try
-//		{
-//			
-//		} 
-//		catch (KeeperException e)
-//		{
-//			e.printStackTrace();
-//		} 
-//		catch (InterruptedException e)
-//		{
-//			e.printStackTrace();
-//		}
-	}
-
-	private void unlock(String arg)
-	{
-		WriteLock lock = new WriteLock(zk, arg, null);
-		
-		System.out.println("Liberando lock " + arg + "...");
-		lock.unlock();
-		System.out.println("Lock " + arg + " liberado");
-	}
-
-	private void lock(String arg)
-	{
-		WriteLock lock = new WriteLock(zk, arg, null);
-		
-		if (lock.isOwner())
-			System.out.println("Você já possui o lock " + arg);
-		else
-		{
-			try
-			{
-				System.out.println("Obtendo lock " + arg + "...");
-				
-				if (lock.lock())
-					System.out.println("Lock " + arg + " obtido com sucesso");
-				else
-					System.out.println("Lock " + arg + " não foi obtido");
-				
-				if (lock.isOwner())
-					System.out.println("Sou dono do lock " + arg);
-				else
-					System.out.println("Sou NAO dono do lock " + arg);		
-			} 
-			catch (KeeperException e)
-			{
-				e.printStackTrace();
-			} 
-			catch (InterruptedException e)
-			{
-				e.printStackTrace();
-			}
-		}
-	}
+		    } 
+		catch (InterruptedException e)
+		    {
+			e.printStackTrace();
+		    }
+	    }
+    }
 }
