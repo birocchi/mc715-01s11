c9b2a42a43dd01dd960eabc04a865f5ebf0fec8d
andreesteve <andreesteve@aa922603-347c-9e05-6bf4-27d00d4c72f5>
2011-03-23 02:43:15 +0000

    Inicio desenvolvimento cliente lock

--- zk_lock_imp/src/zk_lock/imp/Main.java
+++ zk_lock_imp/src/zk_lock/imp/Main.java
@@ -1,15 +1,49 @@
 package zk_lock.imp;
 
-import org.apache.zookeeper.recipes.lock.WriteLock;
+import java.io.IOException;
+import zk_lock.imp.client.Client;
 
 public class Main
 {
-
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args)
 	{
-		WriteLock wl = new WriteLock(null, "a", null);
+		if (args.length < 2)
+		{
+			System.out.println("Usage: zk_lock_imp <zk_host> <port>");
+			System.exit(1);
+		}
+		
+		String host = args[0];
+		int port;
+		Client c;
+		
+		try
+		{
+			port = Integer.parseInt(args[1]);
+			c = new Client(new String[] { host } , port);
+
+			// esperamos cliente morrer
+			while (!c.getDead())
+				Thread.sleep(1000);
+			
+			System.exit(0);
+		}
+		catch(NumberFormatException e)
+		{
+			System.out.println("Porta inválida.");
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();			
+		} 
+		catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+				
+		System.exit(2);
 	}
 }
---
+++ zk_lock_imp/src/zk_lock/imp/client/Client.java
@@ -0,0 +1,88 @@
+package zk_lock.imp.client;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+public class Client implements Watcher
+{
+	private ZooKeeper zk;
+	private volatile boolean dead;	
+	
+	/* properties */
+	
+	public boolean getDead()
+	{
+		return dead;
+	}
+
+	/* constructor */
+	
+	public Client(String[] hosts, int port) throws IOException
+	{
+		StringBuilder connString = new StringBuilder();
+		
+		for(String h : hosts)
+			connString.append(h + ":").append(port).append(",");
+		
+		connString.setLength(connString.length() - 1);		
+		
+		zk = new ZooKeeper(connString.toString(), 3000, this);
+		
+		dead = false;
+	}
+		
+	@Override
+	public void process(WatchedEvent event)
+	{
+		if (event.getType() == Event.EventType.None)
+		{
+			// estado da conexao mudou
+			switch(event.getState())
+			{
+				case SyncConnected:
+					// conectamos
+					// podemos iniciar
+					run();
+					break;
+					
+				case Expired:
+					// sessao expirada
+					dead = true;
+					break;
+			}
+		}
+		else
+		{
+			// algo no nó mudou
+		}
+	}
+	
+	/**
+	 * Starts this client.
+	 */
+	private void run()
+	{
+		try
+		{
+			List<String> children = zk.getChildren("/", false);
+			
+			for (String c : children)
+				System.out.println(c);
+		} 
+		catch (KeeperException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		catch (InterruptedException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
