a3670f7144799f468354d38cc3689a4bea935d80
andreesteve <andreesteve@aa922603-347c-9e05-6bf4-27d00d4c72f5>
2011-05-31 14:50:52 +0000

    Coordenador implementado. Faltam testes

--- zk_tpcp/src/org/apache/zookeeper/recipes/tpcp/CoordinatorTransaction.java
+++ zk_tpcp/src/org/apache/zookeeper/recipes/tpcp/CoordinatorTransaction.java
@@ -1,9 +1,14 @@
 package org.apache.zookeeper.recipes.tpcp;
 
 import java.io.Serializable;
+import java.util.Hashtable;
 import java.util.List;
 
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher.Event.EventType;
+
+import sun.security.util.Debug;
 
 /**
  * Transaction viewed by the coordinator
@@ -11,22 +16,163 @@ import org.apache.zookeeper.WatchedEvent;
  */
 class CoordinatorTransaction extends BaseTransaction 
 {
-	private List<GroupMember> participants;
+	private Object syncLock;
+	private Hashtable<String, TransactionState> participants;
+	
+	/**
+	 * How many participants have already voted.
+	 * **** ATENTION: Change this only when in sync syncLock ****
+	 */
+	private long participantVotes;
+	
+	/**
+	 * How many participants have already joined this transaction
+	 * **** ATENTION: Change this only when in sync syncLock ****
+	 */
+	private long participantsGreeted;
+	
+	/**** ATENTION: Change this.state only when in sync syncLock ****/
 	
 	public CoordinatorTransaction(Serializable query, GroupMember coordinator,
 			List<GroupMember> participants) throws InterruptedException 
 	{
 		super(query, coordinator, participants);
 		
-		this.participants = participants;
+		this.participants = new Hashtable<String, TransactionState>(participants.size());
+		
+		// initialize all participants with 'preset' state to indicate that they have not yet joined the transaction
+		for (GroupMember gm : participants)
+			this.participants.put(gm.getName(), TransactionState.PRESET);
+		
+		this.participantVotes = 0;
+		this.participantsGreeted = 0;
+		
+		this.syncLock = new Object();
 	}
 	
+	private void updateParticipantState(String participantID) throws InterruptedException
+	{
+		TransactionState oldState;
+		TransactionState newState;
+		
+		synchronized (this.syncLock)
+		{
+			// if our (coordinator's) state is SET, there's no decision yet
+			// may there be a case where coordinator have already decided, but there's still votes not seen
+			// in that case, we can safely ignore new votes
+			if (state != TransactionState.SET)
+			{
+				return;
+			}
+			
+			 oldState = participants.get(participantID);
+		}
+
+		if (oldState == null)
+		{
+			Debug.println("Coordinator: ", "Invalid participant '"+participantID+"' on transaction '"+zNodePath+"'");
+			return;
+		}
+		else if (oldState == TransactionState.ABORTED || oldState == TransactionState.PRE_COMMITTED)
+		{
+			// the watcher was left alone - we should ignore this
+			// or we are looking at children list and getting new joined participants while others have already voted 
+			// TODO: improve left alone watcher - i.e. find a way to avoid setting watcher when it's not really needed
+			return;
+		}
+		
+		// from this line, the participant oldState is one of { PRESET, SET } and newState is possibly one of { SET, PRE_COMMITED, ABORTED }   
+		
+		// it's up to me to retrieve the znode state
+		// we can't a priori know the participant actual state thus we can't know if we should set a watch or not
+		// TODO: there's an issue here: left alone watcher - find a way to avoid the last watch (which is left alone) 
+		newState = TransactionState.parse(readParticipantNode(getPath(participantID), true));
+
+		synchronized (this.syncLock)
+		{
+			// avoid any unexpected issue
+			oldState = participants.get(participantID);
+			
+			if (oldState == TransactionState.PRESET || oldState == TransactionState.SET)
+			{
+				participants.put(participantID, newState);
+				
+				// if this is the first time a participant shows up, we should count him in
+				if (oldState == TransactionState.PRESET)
+					participantsGreeted++;
+			}
+			
+			// if someone casts a vote, we should see if we can reach a decision
+			
+			// if our (coordinator's) state is SET, there's no decision yet 
+			if (state == TransactionState.SET)
+			{
+				try
+				{
+					if (newState == TransactionState.PRE_COMMITTED)
+					{
+						// have to wait for other's votes
+						// let's update number of decided nodes
+						participantVotes++;
+											
+						// if everyone has voted, we can decide
+						if (participantVotes == participants.size())
+						{
+							commit(false);
+							decisionReached();
+						}
+					}
+					else if (newState == TransactionState.ABORTED)
+					{
+						// can decide already
+						abort();			
+						decisionReached();
+					}
+				}
+				catch(Exception e)
+				{
+					Debug.println("COORDINATOR: ", "Double decision, prog. error");
+				}
+			}
+		}
+	}
+
 	/**
-	 * Blocking call to coordinate transaction (begin to end)
+	 * Greets new participants
+	 * @return true if every expected participant was greeted at the end of this execution
+	 * @throws InterruptedException
 	 */
+	private boolean greetNewParticipants() throws InterruptedException
+	{
+		boolean everyOneIn = false;
+		
+		try
+		{
+			List<String> children = zkClient.getChildren(zNodePath, getDefaultWatcher());
+			
+			// avoid multiple watcher calls to collide
+			synchronized (this.syncLock)
+			{
+				for (String c : children)
+					updateParticipantState(c);
+				
+				everyOneIn = participantsGreeted == participants.size();
+			}
+		}
+		catch (KeeperException e)
+		{
+			// TODO handle this
+		}
+		
+		return everyOneIn;
+	}
+	
 	private void coordinate() throws InterruptedException
 	{
-		// TODO coordinate
+		// welcome new participants that join transaction
+		greetNewParticipants();
+		
+		// watcher events will do the rest		
 	}
 	
 	@Override
@@ -41,11 +187,48 @@ class CoordinatorTransaction extends BaseTransaction
 			// TODO handle this
 		}
 	}
+	
+	@Override
+	protected void decisionReached()
+	{
+		// free what we don't need anymore
+		participants = null;
+	}
 
 	@Override
 	protected void nodeEvent(WatchedEvent event)
 	{
-		// TODO Auto-generated method stub
+		// coordinator watches for new participants to join and for votes cast
 		
+		String fullPath = event.getPath();
+		String path = fullPath.substring(fullPath.lastIndexOf('/') + 1);
+		
+		EventType etype = event.getType();
+		
+		try
+		{
+			if (path.length() == 0)
+			{
+				// it's transaction root znode
+				// get children changed event
+				if (etype == EventType.NodeChildrenChanged)
+				{
+					greetNewParticipants();					
+				}
+			}
+			else if (path.startsWith(transactionNodePrefix))
+			{
+				// it's a participant node
+				// get data changed event
+				if (etype == EventType.NodeDataChanged)
+				{
+					updateParticipantState(path);
+				}
+			}
+		}
+		catch(InterruptedException e)
+		{
+			// TODO some thing, this is bad :(
+		}
 	}
 }
